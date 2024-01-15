dc2ab3eb91a880d88a1e145695b7817510784484
ebitik1234 <135017212+ebitik1234@users.noreply.github.com>
2024-01-15 18:52:35 +0300

    functions added

--- src/schedule.py
+++ src/schedule.py
@@ -1,6 +1,6 @@
 class Schedule:
     def __init__(self):
-        self.days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]   #Initialization part for class
+        self.days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
         self.weeks = ["Week1"]
         self.time_slots = [
             "8:00 AM - 10:00 AM",
@@ -9,8 +9,12 @@ class Schedule:
             "2:30 PM - 4:30 PM",
             "5:00 PM - 6:00 PM",
             "6:30 PM - 8:30 PM",
-        ] #We defined the possible time slots for the exams
+        ]
         self.schedule = {week: {day: {slot: None for slot in self.time_slots} for day in self.days} for week in self.weeks}
+        
 
+    def assign_exam_time(self, course, day, time_slot, room,week):
+        self.schedule[week][day][time_slot] = f"{time_slot}: {course.course_id} - Room {room.room_id}"
 
-   
+    def block_hours(self, day, start_time, end_time, reason,week):
+        self.schedule[week][day][f"{start_time} - {end_time}"] = f"Blocked: {reason}"
