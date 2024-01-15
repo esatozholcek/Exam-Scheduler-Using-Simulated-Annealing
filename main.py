4e9cae28c60bc25a1a1e321bd8b54bceb666f197
M. Esat Özhölçek <esatozholcek@gmail.com>
2024-01-15 21:50:21 +0300

    input blocked hours from user

--- main.py
+++ main.py
@@ -7,14 +7,55 @@ def day_to_numeric(day):
     days_mapping = {'Monday': 1, 'Tuesday': 2, 'Wednesday': 3, 'Thursday': 4, 'Friday': 5, 'Saturday': 6, 'Sunday': 7}
     return days_mapping[day]
 
+def validate_time_input(time_str):
+    time_slots = [
+        "08:00 AM - 10:00 AM",
+        "10:30 AM - 12:30 PM",
+        "01:00 PM - 02:00 PM",
+        "02:30 PM - 04:30 PM",
+        "05:00 PM - 06:00 PM",
+        "06:30 PM - 08:30 PM",
+    ]
+
+    return time_str in time_slots
+
+def validate_day(day):
+    valid_days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    return day.capitalize() in valid_days
+
 def main():
     class_list = read_class_list('data/class_list.csv')
     classroom_capacities = read_classrooms('data/classroom.csv')
     course_statics = get_course_statistics(class_list)
-    blocked_hours = [
-         ("Wednesday", "2:30 PM", "4:30 PM", "Common Course (TİT102)"),
+    # blocked_hours = [
+    #      ("Wednesday", "02:30 PM", "04:30 PM", "Common Course (TİT102)"),
         
-    ]
+    # ]
+    
+    num_blocked_hours = int(input("Enter the number of blocked hours: "))
+    blocked_hours = []
+    
+    for _ in range(num_blocked_hours):
+        day = input("Enter the day: ")
+        
+        while not validate_day(day):
+            print("Invalid day. Please enter a valid day (e.g., Monday, Tuesday)")
+            day = input("Enter the day: ")
+            
+        start_time = input("Enter the start time (e.g., 02:30 PM): ")
+        end_time = input("Enter the end time (e.g., 04:30 PM): ")
+        
+        time_slot = f"{start_time} - {end_time}"
+
+        while not validate_time_input(time_slot):
+            print("Invalid time. Please enter a valid time slot.")
+            start_time = input("Enter the start time (e.g., 02:30 PM): ")
+            end_time = input("Enter the end time (e.g., 04:30 PM): ")
+            time_slot = f"{start_time} - {end_time}"
+            
+        course_name = input("Enter the course name: ")
+        blocked_hours.append((day.capitalize(), start_time, end_time, course_name))
+
     
     
     scheduler = Scheduler(course_statics, classroom_capacities, blocked_hours)
@@ -23,6 +64,7 @@ def main():
     # Sorting the result based on the day (Monday to Sunday)
     sorted_result = sorted(simulated_annealing_result.items(), key=lambda x: day_to_numeric(x[1]['day']))
 
+    print()
     for index, course_info in sorted_result:
         day = course_info['day'][0:10]
         time_slot = course_info['time_slot']
